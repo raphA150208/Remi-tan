69190629710867ba8e706159231ed5913ee6ed71
satoshi naruki <maptjgpatmpgg1113@gmail.com>
2021-10-13 13:38:41 +0900

    usersテーブルにnameカラムを追加/#34

--- db/schema.rb
+++ db/schema.rb
@@ -31,7 +31,8 @@ ActiveRecord::Schema.define(version: 2021_10_12_085005) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "admin"
+    t.boolean "admin", default: false, null: false
+    t.string "name", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
