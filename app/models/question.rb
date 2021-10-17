9d99f195b056a2925f94a6268cab01568933febd
satoshi naruki <maptjgpatmpgg1113@gmail.com>
2021-10-18 02:55:13 +0900

    statusフォーム編集

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,4 +4,5 @@ class Question < ApplicationRecord
   belongs_to :user
   has_many :reminiscent_words, dependent: :destroy
   accepts_nested_attributes_for :reminiscent_words, allow_destroy: true, reject_if: :all_blank
+  enum status: {未解決:0, 解決済:1}
 end
