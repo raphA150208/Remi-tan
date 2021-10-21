57a589f8dcbc73982ec3adce9d49b6f1daa85d52
satoshi naruki <maptjgpatmpgg1113@gmail.com>
2021-10-22 04:02:17 +0900

    質問投稿アクションのメッセージ修正

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -28,7 +28,7 @@ class QuestionsController < ApplicationController
     else
       respond_to do |format|
         if @question.save
-          format.html { redirect_to @question, notice: "Question was successfully created." }
+          format.html { redirect_to @question, notice: "質問を投稿しました" }
           format.json { render :show, status: :created, location: @question }
         else
           format.html { render :new, status: :unprocessable_entity }
@@ -41,7 +41,7 @@ class QuestionsController < ApplicationController
   def update
     respond_to do |format|
       if @question.update(question_params)
-        format.html { redirect_to @question, notice: "Question was successfully updated." }
+        format.html { redirect_to @question, notice: "質問投稿を編集しました" }
         format.json { render :show, status: :ok, location: @question }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -53,7 +53,7 @@ class QuestionsController < ApplicationController
   def destroy
     @question.destroy
     respond_to do |format|
-      format.html { redirect_to questions_url, notice: "Question was successfully destroyed." }
+      format.html { redirect_to questions_url, notice: "質問投稿を削除しました" }
       format.json { head :no_content }
     end
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,10 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, only: [:show]
-
+  before_action :set_user, only: [:show]
   def show
+    if current_user != @user
+      redirect_to user_path(current_user.id), notice: '他の人のユーザーページです'
+    end
     @my_questions = current_user.questions
     @my_answer_questions = Question.all.find_all{ |q|
       q.answers.any?{ |a| a.user_id == current_user.id }
@@ -20,5 +23,9 @@ class UsersController < ApplicationController
     #     @my_answer_questions.push(q)
     #   end
     # }
+  end
+  private
+  def set_user
+    @user = User.find(params[:id])
   end
 end
